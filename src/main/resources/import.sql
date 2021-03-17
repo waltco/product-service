8fcc5aa207178e2157f211598089b0c9126264e1
Walter Colchado <walter127@gmail.com>
2021-03-17 00:56:17 -0500

    Add import.sql to show data in requests

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO products (name,price,creation_date) VALUES ('Panasonic', 800, NOW());
+INSERT INTO products (name,price,creation_date) VALUES ('Sony', 700, NOW());
+INSERT INTO products (name,price,creation_date) VALUES ('Apple', 1000, NOW());
+INSERT INTO products (name,price,creation_date) VALUES ('Sony Notebook', 500, NOW());
+INSERT INTO products (name,price,creation_date) VALUES ('Hewlett Packard', 600, NOW());
+INSERT INTO products (name,price,creation_date) VALUES ('Bianchi', 100, NOW());
+INSERT INTO products (name,price,creation_date) VALUES ('Nike', 200, NOW());
+INSERT INTO products (name,price,creation_date) VALUES ('Adidas', 300, NOW());
+INSERT INTO products (name,price,creation_date) VALUES ('Reebok', 300, NOW());
