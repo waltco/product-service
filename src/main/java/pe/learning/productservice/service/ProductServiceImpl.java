0e6c85502317be150399fc051bf3e59b0c4a157d
Walter Colchado <walter127@gmail.com>
2021-03-17 00:42:51 -0500

    Fix repository

--- src/main/java/pe/learning/productservice/service/ProductServiceImpl.java
+++ src/main/java/pe/learning/productservice/service/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package pe.learning.productservice.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,10 +10,10 @@ import pe.learning.productservice.repository.ProductRepository;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
-    @Autowired
-    ProductRepository productRepository;
+    final ProductRepository productRepository;
 
     @Override
     @Transactional(readOnly = true)
