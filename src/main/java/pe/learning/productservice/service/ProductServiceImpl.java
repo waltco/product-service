75e7e890adaa182db18280cf554f06631302b47b
Walter Colchado <walter127@gmail.com>
2021-03-17 20:08:28 -0500

    using port as value

--- src/main/java/pe/learning/productservice/service/ProductServiceImpl.java
+++ src/main/java/pe/learning/productservice/service/ProductServiceImpl.java
@@ -3,6 +3,7 @@ package pe.learning.productservice.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,19 +19,20 @@ public class ProductServiceImpl implements ProductService {
 
     final ProductRepository productRepository;
 
-    final Environment environment;
+    @Value("${server.port}")
+    private String port;
 
     @Override
     @Transactional(readOnly = true)
     public List<Product> findAll() {
-        log.info(environment.getProperty("local.server.port"));
+        log.info(port);
         return productRepository.findAll();
     }
 
     @Override
     @Transactional(readOnly = true)
     public Product findById(Long id) {
-        log.info(environment.getProperty("local.server.port"));
+        log.info(port);
         return productRepository.findById(id).orElse(new Product());
     }
 }
